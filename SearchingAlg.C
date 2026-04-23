f73d3d17ec196f983891be55957ac815b972fdda
Alvin Mutahi <143215424+Alvington@users.noreply.github.com>
2026-04-23 19:55:13 +0300

    Add files via upload
    Added 2 files, done in C and Python

---
+++ SearchingAlg.C
@@ -0,0 +1,112 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+
+int linearSearch(int arr[], int size, int key) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == key) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int binarySearch(int arr[], int size, int key) {
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2; //middle index
+
+        if (arr[mid] == key) {
+            return mid; 
+        } else if (arr[mid] < key) {
+            left = mid + 1; // right half
+        } else {
+            right = mid - 1; // left half
+        }
+    }
+    return -1; 
+}
+
+int hashFunction(int key, int size) {
+    return key % size;
+}
+int hashSearch(int arr[], int size, int key) {
+    int index = hashFunction(key, size);
+    if (arr[index] == key) {
+        return index; 
+    }
+    return -1; 
+}
+
+int main() {
+    printf("Enter the size of the array: ");
+    int size;
+    scanf("%d", &size);
+    int arr[size];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < size; i++) {
+       scanf("%d", &arr[i]);
+    }
+    printf("Enter the key to search: ");
+    int key;
+    scanf("%d", &key);
+        
+    //Create a menu
+    printf("    Menu\n");
+    printf("-------------------\n");
+    printf("a. Linear Search\n");
+    printf("b. Binary Search\n");
+    printf("c. Hash Search\n");
+    printf("q. Quit\n");
+    printf("Enter your choice: ");
+    char choice;
+    scanf(" %c", &choice);
+
+    while (choice != 'q') // 'q' for quit
+    {
+        if (choice == 'a') {
+            clock_t start = clock();
+            int linearResult = linearSearch(arr, size, key);
+            clock_t end = clock();
+            double timeTaken = ((double)(end - start) / CLOCKS_PER_SEC) * 1000000; // Convert to microseconds
+            if (linearResult != -1) {
+                printf("Element found at index: %d\n", linearResult);
+            } else {
+                printf("Element not found in the array.\n");
+            }
+            printf("Time taken: %.2f microseconds\n\n", timeTaken);
+        }
+        else if (choice == 'b') {
+            clock_t start = clock();
+            int binaryResult = binarySearch(arr, size, key);
+            clock_t end = clock();
+            double timeTaken = ((double)(end - start) / CLOCKS_PER_SEC) * 1000000; // Convert to microseconds
+            if (binaryResult != -1) {
+                printf("Element found at index: %d\n", binaryResult);
+            } else {
+                printf("Element not found in the array.\n");
+            }
+            printf("Time taken: %.2f microseconds\n\n", timeTaken);
+        }
+        else if (choice == 'c') {
+            clock_t start = clock();
+            int hashResult = hashSearch(arr, size, key);
+            clock_t end = clock();
+            double timeTaken = ((double)(end - start) / CLOCKS_PER_SEC) * 1000000; // Convert to microseconds
+            if (hashResult != -1) {
+                printf("Element found at index: %d\n", hashResult);
+            } else {
+                printf("Element not found in the array.\n");
+            }
+            printf("Time taken: %.2f microseconds\n\n", timeTaken);
+        }
+        else {
+            printf("Invalid choice. Please try again.\n");
+        }
+        printf("Enter your choice: ");
+        scanf(" %c", &choice);
+    }
+    return 0;
+}
---
+++ assignment.py
@@ -0,0 +1,75 @@
+import time
+numbers = []
+def linearSearch(array, key):
+    start = time.time()
+    for i in range (0, len(array)):
+        if array[i] == key:
+            print(f"{linearValue} found at index {numbers.index(linearValue)}\n")
+    
+    if key not in array:
+            print(f"{linearValue} not found\n")
+    
+    end = time.time()
+    total = start - end
+    print(f"Linear total search time = {total}\n")
+
+
+def binarySearch(array, key):
+    start = time.time()
+    array.sort()
+    low = 0
+    high = len(array) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        if array[mid] > key:
+            high = mid - 1
+        elif array[mid] < key:
+            low = mid + 1
+        else:
+            print(f"{key} found at index {mid}\n")
+            return
+        
+    print(f"{key} not found\n")
+    end = time.time()
+    total = start - end
+    print(f"Binary total search time = {total}")
+
+
+def hashing(array, key):
+    start = time.time()
+    if key in array:
+        index = key % len(array)
+        print(f"{key} found at index {index}\n")
+    else:
+        print(f"{key} not found\n")
+
+    end = time.time()
+    total = start - end
+    print(f"Hashing total search time = {total}")
+
+while True:
+    num = int(input("Enter the number you want to add to the array (-1 to exit): "))
+    if num == -1:
+        break
+    else:
+        numbers.append(num)
+
+print()
+
+
+while True:
+    print("1.Linear Search\n2.Binary Search\n3.Hashing\n4.Exit")
+    choice = int(input("\nMake your choice: "))
+
+    if choice == 1:
+        linearValue = int(input("Which number would you like to search for: "))
+        linearSearch(numbers, linearValue)
+    elif choice == 2:
+        binaryValue = int(input("Which number would you like to search for: "))
+        binarySearch(numbers, binaryValue)
+    elif choice == 3:
+        hashValue = int(input("Which number would you like to search for: "))
+        hashing(numbers, hashValue)
+    else:
+        break
